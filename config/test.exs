80d697fd6184387da7fa5053426e419ea1ec345c
Sasha Moak <sasha.moak@gmail.com>
2018-11-23 14:31:17 -0800

    Adding a method to generate the request signature and a test case

--- config/test.exs
+++ config/test.exs
@@ -8,3 +8,7 @@ config :teamcowboygraphql, TeamCowboyGraphQLWeb.Endpoint,
 
 # Print only warnings and errors during test
 config :logger, level: :warn
+
+config :teamcowboygraphql, :teamcowboy_config,
+  private_api_key: "413abdc2120adb9a06eb13cf76483aa25d18dc5a",
+  public_api_key: "foo"
--- lib/teamcowboygraphql_web/resolvers/teams.ex
+++ lib/teamcowboygraphql_web/resolvers/teams.ex
@@ -1,7 +1,5 @@
 defmodule TeamCowboyGraphQLWeb.Resolvers.Teams do
   def list(_parent, args, %{context: %{user_token: user_token}}) do
-    args |> IO.inspect()
-    user_token |> IO.inspect()
     {:ok, []}
   end
 
