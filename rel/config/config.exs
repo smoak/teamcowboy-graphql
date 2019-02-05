4780f79c2ce48457021128c8fe9af1f65432040c
Sasha Moak <sasha.moak@gmail.com>
2019-02-04 22:41:20 -0800

    Allow HOSTNAME and PORT to be passed in.
    Trying to get gigalixir working

--- rel/config/config.exs
+++ rel/config/config.exs
@@ -1,17 +1,18 @@
 use Mix.Config
 
-port = 443
+port = String.to_integer(System.get_env("PORT"))
 app_name = System.get_env("APP_NAME")
 
 config :teamcowboygraphql, TeamCowboyGraphQLWeb.Endpoint,
   http: [:inet6, port: port],
-  url: [host: "#{app_name}.gigalixirapp.com", port: port],
+  url: [host: System.get_env("HOSTNAME"), port: port],
   secret_key_base: System.get_env("SECRET_KEY_BASE")
-  # https: [
-  #   :inet6,
-  #   port: System.get_env("PORT") || 443,
-  #   cipher_suite: :strong,
-  #   keyfile: System.get_env("HTTP_SSL_KEY_PATH"),
-  #   certfile: System.get_env("HTTP_SSL_CERT_PATH")
-  # ],
-  # force_ssl: [rewrite_on: [:x_forwarded_proto], hsts: true],
+
+# https: [
+#   :inet6,
+#   port: System.get_env("PORT") || 443,
+#   cipher_suite: :strong,
+#   keyfile: System.get_env("HTTP_SSL_KEY_PATH"),
+#   certfile: System.get_env("HTTP_SSL_CERT_PATH")
+# ],
+# force_ssl: [rewrite_on: [:x_forwarded_proto], hsts: true],
