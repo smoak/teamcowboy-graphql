7594a22159c094f04c253996f6872a3a8e12f0dd
Sasha Moak <smoak@microsoft.com>
2019-03-26 19:22:15 -0600

    Move the cors config to prod
    Leave the dev cors config alone so it responds with all cors options for
    local development

--- config/config.exs
+++ config/config.exs
@@ -30,11 +30,6 @@ config :teamcowboygraphql, :teamcowboy_config,
   public_api_key: System.get_env("TC_PUBLIC_API_KEY"),
   private_api_key: System.get_env("TC_PRIVATE_API_KEY")
 
-config :cors_plug,
-  origin: ["https://team-cowgirl.surge.sh"],
-  max_age: 86400,
-  methods: ["GET", "POST"]
-
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
--- config/prod.exs
+++ config/prod.exs
@@ -2,6 +2,11 @@ use Mix.Config
 
 config :teamcowboygraphql, TeamCowboyGraphQLWeb.Endpoint, server: true
 
+config :cors_plug,
+  origin: ["https://team-cowgirl.surge.sh"],
+  max_age: 86400,
+  methods: ["GET", "POST"]
+
 config :logger,
   level: :info,
   handle_sasl_reports: true,
